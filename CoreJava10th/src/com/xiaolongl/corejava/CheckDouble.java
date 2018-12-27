3fd3255dd83ed428bf9cbd93bd49f98cc33d7e87
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-27 14:11:41 -0500

    Math.round

---
+++ CoreJava10th/src/com/xiaolongl/corejava/CheckDouble.java
@@ -0,0 +1,17 @@
+package com.xiaolongl.corejava;
+
+public class CheckDouble
+{
+	public static void main(String[] args)
+	{
+		double a = 100.1;
+		double b = 100.4;
+
+		double c = Math.round(a);
+		double d = Math.round(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
