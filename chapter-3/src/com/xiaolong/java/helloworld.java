3420c4c0dac3f93ca77acf65018a5743a9b2c6ec
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-28 14:39:09 -0500

    Unicode&ASCII

---
+++ chapter-3/src/com/xiaolong/java/helloworld.java
@@ -0,0 +1,25 @@
+package com.xiaolong.java;
+
+public class helloworld
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("Hello world!");
+		//1. ASCII
+		int num  = 97;
+		char c = (char) num;
+		System.err.println(c);
+		
+		//2. Unicode
+		
+		int num2 = 0x3425;
+		char c2 = (char)num2;
+		System.err.println(c2);
+		
+		
+	}
+	
+	
+
+
+}
